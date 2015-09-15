207c90fa8297b14d115d7baad5db5e97f9888e1b
user <user@user-PC>
2015-09-15 17:16:21 +0900

    2105.09.15 변수

--- java-algo/src/hello/GoodMorning.java
+++ java-algo/src/hello/GoodMorning.java
@@ -2,7 +2,7 @@ package hello;
 
 public class GoodMorning {
 	public static void main(String[] args) {
-		String goodMorning = "굿모닝";
+		String goodMorning = "굿모닝써";
 		System.out.println(goodMorning);
 	}
 
