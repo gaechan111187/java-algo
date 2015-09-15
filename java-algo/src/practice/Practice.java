93d80558aa4f49658b5aaed379e21ecda04035e9
user <user@user-PC>
2015-09-15 17:54:02 +0900

    2015.9.15 변수 최종

--- java-algo/src/practice/Practice.java
+++ java-algo/src/practice/Practice.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Practice {
 	public static void main(String[] args) {
 		System.out.println("지방의 그램을 입력하세요");
-		Scanner fat = new Scanner(System.in);
+		Scanner f = new Scanner(System.in);
+		String fa = f.next();
 		System.out.println("탄수화물의 그램을 입력하세요");
-		Scanner tan = new Scanner(System.in);
+		Scanner t = new Scanner(System.in);
+		String ta = t.next();
 		System.out.println("단백질의 그램을 입력하세요");
-		Scanner dan = new Scanner(System.in);
-		
-						
-		
-		
-		
+		Scanner d = new Scanner(System.in);
+		String da = d.next();
+		String s = da + fa + ta;
+		System.out.println("총칼로리는 "+s+"cal 입니다.");
 				
 	}
 
